3e6927e84d28d441089f2621bbacc66a6b6d206c
rakshithaghorpade <53851629+rakshithaghorpade@users.noreply.github.com>
2019-10-01 14:40:07 +0530

    Add files via upload

---
+++ gameboards.py
@@ -0,0 +1,12 @@
+for i in range(3):
+    for j in range(3):
+        print(" ",end="")
+        print('-'*3,end="")
+        print(" ",end="")
+
+        print()
+        print("| "*4)
+for j in range(3):
+    print(" ",end="")
+    print('-'*3,end="")
+    print(" ",end="")
